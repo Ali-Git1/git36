34bf45bbdbbf676f362e6f6d485c87a62b7c5f52
Ali Aliyev <elvinaliyev724@gmail.com>
2024-04-29 16:19:31 +0400

    finish

---
+++ ConsoleApp34/Exam.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp34
+{
+    public class Exam
+    {
+       
+        public string Student { get; set; }
+
+        public int Point { get; set; }
+
+
+        public string Subject { get; set; }
+
+
+        public DateTime StartDate { get; set; }
+
+
+        public DateTime EndDate { get; set; }
+
+
+
+        
+
+
+    }
+}
---
+++ ConsoleApp34/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp34
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Exam> examlist = new List<Exam>();
+
+
+            Console.WriteLine("Daxil etmet istediyiniz exam sayini yazin");
+            int girilendeyer = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < girilendeyer; i++)
+            {
+                Exam exam1 = new Exam();
+
+                Console.Write("Studenti daxil edin: ");
+                exam1.Student = Console.ReadLine();
+
+
+                Console.Write("Bal  daxil edin:");
+                exam1.Point = int.Parse(Console.ReadLine());
+
+                Console.Write("Imtahan adini daxil edin:");
+                exam1.Subject = (Console.ReadLine());
+
+                Console.Write("Baslangic vaxti  daxil edin:");
+                exam1.StartDate = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Cixis vaxtini daxil edin:");
+                exam1.EndDate = DateTime.Parse(Console.ReadLine());
+
+                examlist.Add(exam1);
+
+            }
+
+            foreach (var exam1 in examlist)
+            {
+                if (exam1.Point > 50)
+                {
+                    Console.WriteLine($"{exam1.Student} {exam1.Subject} {exam1.Point}");
+                }
+            }
+
+            foreach (var exam1 in examlist)
+            {
+                if (exam1.EndDate - exam1.StartDate > new TimeSpan(1, 0, 0))
+                {
+                    Console.WriteLine($"{exam1.Student} {exam1.Subject} {exam1.StartDate} {exam1.EndDate}");
+                }
+            }
+
+        }
+    }
+}
+
